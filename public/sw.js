a843cb9040e18db44be6b37b8c5a8cf1ba34a819
Zeeshan Raza <42145844+ZeeshanRaza25@users.noreply.github.com>
2020-09-06 20:29:27 +0500

    updated

--- public/sw.js
+++ public/sw.js
@@ -1,44 +1,67 @@
-console.log('registered');
-let cacheData = 'appv1';
+/* eslint-disable */
 
-// var urlsToCache = [
-//   '/',
-//   '/styles/main.css',
-//   '/script/main.js'
-// ];
+// console.log('registered');
+let cacheData = 'app1';
 
-this.addEventListener('activate', (event) => {
+var urlsToCache = [
+  '/',
+  'localhost',
+  '/static/js/1.chunk.js',
+  '/static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/0.bundle.js',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/images/icons/test-192x192.png',
+  // '/main.52182c7192c981271d21.hot-update.js',
+  // 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
+];
+  
+//   window.addEventListener('install', function (e) {
+
+//   e.userChoice.then(function (choiceResult) {
+//     console.log(choiceResult.outcome);
+//     if (choiceResult.outcome === 'dismissed') {
+//       console.log('User cancelled home screen install');
+//     } else {
+//       console.log('User added to home screen');
+//     }
+//   });
+// });
+
+self.addEventListener('activate', (event) => {
   // Perform install steps
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        // 'localhost',
-        '/static/js/1.chunk.js',
-        '/static/js/bundle.js',
-        '/static/js/main.chunk.js',
-        '/static/js/0.chunk.js',
-        '/static/js/0.bundle.js',
-        '/index.html',
-        '/manifest.json',
-        '/favicon.ico',
-        '/images/icons/test-192x192.png',
-        // '/main.52182c7192c981271d21.hot-update.js',
-        // 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
-        '/',
-      ]);
+      cache.addAll(urlsToCache);
     })
   );
 });
 
-this.addEventListener('fetch', (event) => {
-  if (!navigator.onLine) {
+self.addEventListener(
+  'fetch',
+  (event) => {
+    console.log(event.request.url);
+    // if (!navigator.onLine) {
     event.respondWith(
       caches.match(event.request).then((result) => {
         console.log('result', result);
         if (result) {
-          return result;
+          return result || fetch(event.request);
         }
       })
     );
   }
-});
+  // }
+);
+
+// self.addEventListener('fetch', function(event) {
+//   console.log(event.request.url);
+//   event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//           return response || fetch(event.request);
+//       })
+//   );
+// });
