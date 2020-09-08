6cb2f57b42f3205f13b60405bfb6f727bf999c73
Zeeshan Raza <42145844+ZeeshanRaza25@users.noreply.github.com>
2020-09-08 20:59:19 +0500

    remove console

--- public/sw.js
+++ public/sw.js
@@ -26,7 +26,7 @@ this.addEventListener('install', (event) => {
 this.addEventListener(
   'fetch',
   (event) => {
-    console.log(event.request.url);
+    // console.log(event.request.url);
     event.respondWith(
       caches.match(event.request).then((result) => {
         if (result) {
